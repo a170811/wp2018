29714690ee27521682f1496ef58dee493fc2c325
a170811 <a170811@gmail.com>
2018-10-04 18:03:32 +0800

    refine animation-by jquery : done

--- 1_intro/index.js
+++ 1_intro/index.js
@@ -1,14 +1,16 @@
 function content(status){
     var title = $('#title') ;
     var gallery = $('#gallery') ;
-    var move = 20 ;
+    var move = 40 ;
     if(status==1) {
-        title.animate({margin:`-=${move}px auto +=${move}px auto`, opacity:'0'}, 800) ;
-        gallery.animate({marginTop:`${2*move}px`,opacity:'0'}, 800) ;
+        title.animate({margin:`-=${move}px auto +=${move}px auto`, opacity:'0'}, 600) ;
+        gallery.animate({marginTop:`${2*move}px`,opacity:'0'}, 600) ;
     }
     else {
-        title.animate({marginTop:'0px', display:'block'}, 300) ;
-        gallery.animate({paddingTop:'0px', display:'block'}, 300) ;
+        title.stop();
+        gallery.stop();
+        title.css({'margin':'3vh auto', 'opacity':'1'}) ;
+        gallery.css({'marginTop':'0', 'opacity':'1'}) ;
     }
 }
 
@@ -16,31 +18,47 @@ function content(status){
 function img_enlarge(status, element){
     var offset = $('#container').offset();
     var j_ele = $(element);
+    var info = $('#info') ;
+    var info_container = $('#info_container')
     if(status==1) {
-        $('#info_container').css({ 'display':'block' });
+        var top = `${j_ele.offset().top-offset.top}px`;
+        var left = `${j_ele.offset().left-offset.left}px`;
+        var width = `${j_ele.width()}px`;
+        var height = `${j_ele.height()}px`;
         var id = element.getAttribute('id');
-        var div = $(`<div id='show_img' class='c_border' style='
-            position:absolute;
-            top:0px;
-            left:${j_ele.offset().left-offset.left}px;
-            width:${j_ele.width()}px;
-            height:${j_ele.height()}px;
-            '></div>`).appendTo('#info_container'); 
-        div.css({'background-image':`url('./src/${id}.JPG')`, 'background-size':'cover' });
-        var info_container = $('#info_container')
-        info_container.css({ 'top':`${j_ele.offset().top-offset.top}px` });
-
-
-        var info = $('#info') ;
-        $('#arrow').appendTo('#show_img') ;
+        info_container.css({ 'display':'block' });
         $('#info_title').html(id) ;
+        info_container.css({ 'top':top });
 
+        if( $('#show_img').length == 0 ) {
+        
+            var div = $(`<div id='show_img' class='c_border' style='
+                position:absolute;
+                top:0px;
+                left:${left};
+                width:${width};
+                height:${height};
+                '></div>`).appendTo('#info_container'); 
+            div.css({'background-image':`url('./src/${id}.JPG')`, 'background-size':'cover' });
+            $('#arrow').appendTo('#show_img') ;
+        
+        }
         //pic and container
         info_container.animate({ top:'0px' }, 500 , 'easeInOutCubic');
         div.animate({top:'0px', width:'100%', left:'0px', height:'30vh'}, 500 ,'easeInOutCubic');
         
         //info block
         info.animate({top:'25vh'}, 400 , 'easeInOutQuart');
+    }
+    else {
+        
+        $('#arrow').appendTo(info_container) ;
+        info_container.animate({top:'-300px', opacity:'0'}, 500, function(){
+            $('#show_img').remove();
+            this.style.opacity = 1;
+            info_container.css({ 'display':'none' });
+        });
+        
     }
 }
 
@@ -50,93 +68,9 @@ $(document).ready(()=>{
         content(1);
         img_enlarge(1, this);
     });
+    $('#arrow').click(function(){
+        content(2);
+        img_enlarge(2);
+    });
 });
 
-/*
-//will delete
-function Cursor_pos() {
-    var x = event.clientX ;
-    var y = event.clientY ;
-    return [ x, y]
-}
-
-function move_img_block( status ) {
-
-    var block = document.getElementById('show_img') ;
-    if(status==1) {
-        block.classList.add('move_img_block') ;
-        block.style.display = 'block' ;
-    }
-    else {
-        block.classList.remove('move_img_block') ;    
-        block.style.display = 'none' ;
-    }
-}
-
-function show_pic( status , element , callback ) {
-
-    var img = document.getElementById('show_img') ;
-    img.style.position = 'absolute' ;
-    img.style.zIndex = '2' ;
-
-    if(status == 1) {
-        var rect = element.getBoundingClientRect() ;
-        var pic = element.getAttribute('id') ;
-        img.childNodes[1].style.backgroundImage = "url('./src/"+pic+".JPG')" ;
-        img.childNodes[1].style.backgroundImage = "url('./src/"+pic+".JPG')" ;
-        var pos = [ rect.top , rect.left , (rect.right-rect.left) , (rect.bottom-rect.top) ]
-        document.documentElement.style.setProperty('--top' , pos[0]) ;
-        document.documentElement.style.setProperty('--left' , pos[1]) ;
-        document.documentElement.style.setProperty('--width' , pos[2]) ;
-        document.documentElement.style.setProperty('--height' , pos[3]) ;
-        img.childNodes[1].classList.add('show_pic') ;
-    }
-    else {
-        img.childNodes[1].classList.remove('show_pic') ;
-    }
-
-    if (typeof callback == 'function') {
-        callback() ;
-    }
-}
-
-function show_info( status ,id_name ) {
-
-    var info = document.getElementById('info') ;
-    if (status==1) {
-        info.classList.add('show_info') ;
-        document.getElementById('info_title').innerHTML = id_name ;
-    }
-    else {
-        info.style.height = 0 ;
-        info.classList.remove('show_info') ;
-    }
-}
-
-function fadeOut( status ) {
-    var title = document.getElementById('title') ;
-    var favorite = document.getElementById('favorite') ;
-    if(status==1) {
-        title.classList.add('fadeUp') ;
-        favorite.classList.add('fadeDown') ;
-    }
-    else {
-        title.classList.remove('fadeUp') ;
-        favorite.classList.remove('fadeDown') ;
-    }
-
-}
-
-pic = document.getElementsByClassName('pic') ;
-for(let i of pic) {
-    i.onclick = ( )=> { 
-    } ;
-}
-arrow = document.getElementById('arrow') ;
-arrow.onclick = ()=>{
-    show_info(2) ;
-    fadeOut(2) ;
-    move_img_block(2) ;
-    show_pic( 2, arrow , show_info ) ;
-}
-*/
