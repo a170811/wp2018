3057acc1d5cd2b4f37b7c83da5086b8e98f76a2a
a170811 <a170811@gmail.com>
2018-10-04 23:47:41 +0800

    prac express

---
+++ my_project/public/index.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {  
+    $("#ajax_form button").click(function(event) {
+        event.preventDefault();//取消reload
+        
+        $.ajax({
+            method: "get",
+            url: "./ajax_data",
+            data: {
+                fname: $("#ajax_form input[name='fname']").val(),
+                lname: $("#ajax_form input[name='lname']").val()
+            },
+            success: function(data) {
+                $("#ajax_content").text(data);
+            }
+        })
+        $("#ajax_content").text('loading');
+    })  
+})
--- my_project/server.js
+++ my_project/server.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 15555
+const express = require('express');
+const app = express();
+const port = 15555;
 
 app.get('/get_data' , function(req, res) {
-    res.send(`<h1>Hello, ${req.query.fname} ${req.qurey.lname}</h1>`)
-})
+    res.send(`<h1>Hello, ${req.query.fname} ${req.query.lname}</h1>`);
+});
 
+app.get('/ajax_data', function(req, res){
+    res.send(`<h1>Hello, ${req.query.fname} ${req.query.lname}</h1>`);
+});
 
-app.use(express.static(__dirname + '/public'))
-app.listen(port)
-console.log(port)
+app.use(express.static(__dirname + '/public'));
+app.listen(port);
+console.log(port);
 
